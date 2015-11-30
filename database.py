e401137fb0d1e65b9aedc8fb4398c96616b7567c
Jamie Bird <jamie06bird@gmail.com>
2015-11-30 14:57:20 +0000

    Fixed indentation bug. Up-to-date with forge repo.

--- database.py
+++ database.py
@@ -38,18 +38,18 @@ class DatabseStatsMSSQL(checks.AgentCheck):
     		self.prev_stats[name] = row
     		return
 
-   		#set dimensions
-		dimensions = self._set_dimensions({"host": host, "database": database}, instance)
+   	#set dimensions
+	dimensions = self._set_dimensions({"host": host, "database": database}, instance)
 
-		#post differences
-		self.gauge('database.num_reads', row['NumberReads'] - self.prev_stats[name]['NumberReads'],  dimensions)
-		self.gauge('database.bytes_read', row['BytesRead'] - self.prev_stats[name]['BytesRead'],  dimensions)
-		self.gauge('database.io_stall_read', row['IoStallReadMS'] - self.prev_stats[name]['IoStallReadMS'],  dimensions)
-		self.gauge('database.num_writes', row['NumberWrites'] - self.prev_stats[name]['NumberWrites'],  dimensions)
-		self.gauge('database.bytes_written', row['BytesWritten'] - self.prev_stats[name]['BytesWritten'],  dimensions)
-		self.gauge('database.io_stall_writes', row['IoStallWriteMS'] - self.prev_stats[name]['IoStallWriteMS'],  dimensions)
-		self.gauge('database.io_stall', row['IoStallMS'] - self.prev_stats[name]['IoStallMS'],  dimensions)
-		self.gauge('database.bytes_on_disk', row['BytesOnDisk'] - self.prev_stats[name]['BytesOnDisk'],  dimensions)    	
+	#post differences
+	self.gauge('database.num_reads', row['NumberReads'] - self.prev_stats[name]['NumberReads'],  dimensions)
+	self.gauge('database.bytes_read', row['BytesRead'] - self.prev_stats[name]['BytesRead'],  dimensions)
+	self.gauge('database.io_stall_read', row['IoStallReadMS'] - self.prev_stats[name]['IoStallReadMS'],  dimensions)
+	self.gauge('database.num_writes', row['NumberWrites'] - self.prev_stats[name]['NumberWrites'],  dimensions)
+	self.gauge('database.bytes_written', row['BytesWritten'] - self.prev_stats[name]['BytesWritten'],  dimensions)
+	self.gauge('database.io_stall_writes', row['IoStallWriteMS'] - self.prev_stats[name]['IoStallWriteMS'],  dimensions)
+	self.gauge('database.io_stall', row['IoStallMS'] - self.prev_stats[name]['IoStallMS'],  dimensions)
+	self.gauge('database.bytes_on_disk', row['BytesOnDisk'] - self.prev_stats[name]['BytesOnDisk'],  dimensions)    	
 
     	self.prev_stats[name] = row
 
